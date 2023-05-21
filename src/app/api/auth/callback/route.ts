e84a82e0756247e11ff855eddd95f343267d8829
André Loezer <andrecesarloezer@gmail.com>
2023-05-20 21:34:22 -0300

    +Memory creation

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,7 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
+
+  console.log('searchParams', searchParams)
   const code = searchParams.get('code')
+  console.log('code', code)
 
   const redirectTo = request.cookies.get('redirectTo')?.value
 
